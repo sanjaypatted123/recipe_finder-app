99ed40a446d6e9a69f382bb3a1d9b95d99078e92
SANJAY PATTED <sanjaypattedbng@gmail.com>
2025-05-01 15:33:39 +0530

    final with good css styling

---
+++ app.js
@@ -0,0 +1,42 @@
+const apikey='f1624b52d6f04943872da438a4964587';    //apikey taken from spoonacular website
+
+//just accessing all html elements for dom actions
+const button=document.getElementById('search-btn');  
+const input=document.getElementById('ing-input');
+const result=document.getElementById('results');
+//adding a event liostener for click button
+button.addEventListener('click',function () {
+    const ingredient=input.value;
+
+    if (ingredient===''){
+        alert('please enter an ingredient');
+        return;
+    }
+    const url='https://api.spoonacular.com/recipes/findByIngredients?ingredients=' + ingredient + '&number=1&apiKey=' + apikey;
+    fetch(url)
+    .then(function (response) {
+        return response.json();
+
+        
+    })
+    .then(function (data) {
+        if(data.length===0){
+            result.innerHTML='No recipe found';
+            return;
+
+        }
+        const recipe =data[0];
+        result.innerHTML = `
+        <h3>${recipe.title}</h3>
+        <img src="${recipe.image}" alt="${recipe.title}" width="250">
+    `;
+        
+    })
+    .catch(function () {
+        result.innerHTML='Something went wrong.please try again';
+
+        
+    });
+    
+});
+
